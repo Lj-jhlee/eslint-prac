444f3f6e8492f75710636abe9859265868c64337
Lj-jhlee <jhlee@42maru.com>
2021-04-26 11:25:55 +0900

    version test -vum

--- version_script.sh
+++ version_script.sh
@@ -12,22 +12,3 @@ elif [[ $COMMIT_MSG == *"-vup"* ]]
 then
   echo 'PATCH_UPDATE' && npm version patch
 fi
-
-# MAJOR_UPDATE=$(git log --oneline -n 1 HEAD | grep "vuM")
-# MINOR_UPDATE=$(git log --oneline -n 1 HEAD | grep "vum")
-# PATCH_UPDATE=$(git log --oneline -n 1 HEAD | grep "vup")
-
-# echo $MAJOR_UPDATE && echo 'aa' && echo $MINOR_UPDATE && echo 'bb' && echo $PATCH_UPDATE && echo 'cc'
-
-# if [ ${#MAJOR_UPDATE} > 0 ]
-# then
-#   echo 'MAJOR_UPDATE' && npm version major
-
-# elif [ ${#MINOR_UPDATE} > 0 ]
-# then
-#   echo 'MINOR_UPDATE' && npm version minor
-
-# elif [ ${#PATCH_UPDATE} > 0 ]
-# then
-#   echo 'PATCH_UPDATE' && npm version patch
-# fi
